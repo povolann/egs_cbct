812608fb4d447b0a32e57d6a48211f1446447607
povolann <31487206+povolann@users.noreply.github.com>
2018-11-02 22:29:13 +0100

    Update automat.sh

--- automat.sh
+++ automat.sh
@@ -1,22 +1,20 @@
 ﻿#! /bin/bash
 set -x #kontrola promennych
 
-zacatek=694
-konec=717
+start=694
+end=717
 inputFile="/home/anya/EGSnrc/egs_home/egs_cbct/blank_phant.egsinp"
-puvod=$(grep -Po 'initial seeds = 1802 \K[^ ]+' $inputFile)
+origin=$(grep -Po 'initial seeds = 1802 \K[^ ]+' $inputFile)
 
-for ((n=$zacatek; n<=$konec; n++))
+for ((n=$start; n<=$end; n++))
 do
 outputFile="/home/anya/EGSnrc/egs_home/egs_cbct/phant_$n.egsinp"
-cislo=$(grep -Po 'initial seeds = 1802 \K[^ ]+' $outputFile)
-sed -i "s/seeds = 1802 $puvod/seeds = 1802 $cislo/g" $inputFile
-puvod=$cislo
+new=$(grep -Po 'initial seeds = 1802 \K[^ ]+' $outputFile)
+sed -i "s/seeds = 1802 $origin/seeds = 1802 $new/g" $inputFile
+origin=$new
 
-#./egs-parallel.sh 6 
-egs_cbct -i blank_phant
-#./egs-parallel.sh 6 
-egs_cbct -i phant_$n
+./egs-parallel.sh 6 egs_cbct -i blank_phant
+./egs-parallel.sh 6 egs_cbct -i phant_$n
 #prikaz pro provedení MC pro zacatek až konec vcetne obou
 
 done
